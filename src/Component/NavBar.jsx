e55c8c7ea436e441dcdfcede5938c6e2dfe3a163
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-10-30 20:51:51 +0530

    created nav bar

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import logo from '../Assets/images/logo.png';
 
 export const NavBar = () => {
   return (
-    <div className='w-full h-[100px] '></div>
+<div className='w-full h-[100px] bg-slate-200 shadow-xl'>
+<div className='justify-between flex p-2'>
+    <div className='p-2 flex items-center justify-center'>
+        <img src={logo} alt='logo' className='w-[250px] h-full' />
+    </div>
+    <div className=' w-full justify-end flex gap-10 text-center my-auto '>
+        <ul className='items-center flex gap-20'>
+        <li>
+            <a
+              className='text-2xl tracking-wide cursor-pointer duration-500 text-textColor hover:text-[#5e1b1b] transform hover:scale-105 border-b-2 border-transparent hover:border-[#5e1b1b]'
+
+              href="#Services"          
+            >
+              Services
+            </a>
+          </li>
+        <li>
+            <a
+             className='text-2xl tracking-wide cursor-pointer duration-500 text-textColor hover:text-[#5e1b1b] transform hover:scale-105 border-b-2 border-transparent hover:border-[#5e1b1b]'
+              href="#ContactUs"          
+            >
+              Contact Us
+            </a>
+          </li>
+        </ul>
+
+    </div>
+</div>
+</div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['SVN-Gilroy', 'sans-serif'],
+      },
+      
+      screens: {
+        'xs': '414px',
+        'sm': '640px', 
+        'md': '768px',  
+        'lg': '1024px', 
+        'xl': '1280px', 
+        '2xl': '1536px' 
+      },
+      colors:{
+        textColor:'#A03232'
+      }
+    },
+  },
+  plugins: [],
+};
