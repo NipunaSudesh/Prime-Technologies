ea9f9696e1e186059ef900b93028914f1953c351
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-10-31 11:30:26 +0530

    created service card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './Component/NavBar';
 import { Home } from './Page/Home';
+import { Services } from './Page/Services/Services';
 
 function App() {
   return (
     <div className="flex flex-col gap-4">
       <NavBar />
       <Home />
+      <Services />
     </div>
   );
 }
